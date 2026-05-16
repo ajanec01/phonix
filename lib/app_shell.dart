bfe3d161b35aa5fb63b9108757b8bb2baaabded3
ajanec01 <aronjanecki@gmail.com>
2026-05-16 15:22:31 +0100

    Switch to Cupertino icons in bottom nav bar, remove phosphor_flutter

--- lib/app_shell.dart
+++ lib/app_shell.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'features/learn/learn_screen.dart';
 import 'features/library/library_screen.dart';
 import 'features/play/play_screen.dart';
@@ -16,7 +16,7 @@ class AppShell extends StatefulWidget {
 class _AppShellState extends State<AppShell> {
   int _currentIndex = 0;
 
-  static final _screens = [
+  static const _screens = [
     LearnScreen(),
     PracticeScreen(),
     PlayScreen(),
@@ -24,30 +24,30 @@ class _AppShellState extends State<AppShell> {
     ProgressScreen(),
   ];
 
-  static final _destinations = [
+  static const _destinations = [
     NavigationDestination(
-      icon: PhosphorIcon(PhosphorIcons.bookOpen()),
-      selectedIcon: PhosphorIcon(PhosphorIcons.bookOpen(PhosphorIconsStyle.fill)),
+      icon: Icon(CupertinoIcons.book),
+      selectedIcon: Icon(CupertinoIcons.book_fill),
       label: 'Learn',
     ),
     NavigationDestination(
-      icon: PhosphorIcon(PhosphorIcons.pencilSimple()),
-      selectedIcon: PhosphorIcon(PhosphorIcons.pencilSimple(PhosphorIconsStyle.fill)),
+      icon: Icon(CupertinoIcons.pencil_circle),
+      selectedIcon: Icon(CupertinoIcons.pencil_circle_fill),
       label: 'Practice',
     ),
     NavigationDestination(
-      icon: PhosphorIcon(PhosphorIcons.gameController()),
-      selectedIcon: PhosphorIcon(PhosphorIcons.gameController(PhosphorIconsStyle.fill)),
+      icon: Icon(CupertinoIcons.game_controller),
+      selectedIcon: Icon(CupertinoIcons.game_controller_solid),
       label: 'Play',
     ),
     NavigationDestination(
-      icon: PhosphorIcon(PhosphorIcons.books()),
-      selectedIcon: PhosphorIcon(PhosphorIcons.books(PhosphorIconsStyle.fill)),
+      icon: Icon(CupertinoIcons.bookmark),
+      selectedIcon: Icon(CupertinoIcons.bookmark_fill),
       label: 'Library',
     ),
     NavigationDestination(
-      icon: PhosphorIcon(PhosphorIcons.chartBar()),
-      selectedIcon: PhosphorIcon(PhosphorIcons.chartBar(PhosphorIconsStyle.fill)),
+      icon: Icon(CupertinoIcons.graph_circle),
+      selectedIcon: Icon(CupertinoIcons.graph_circle_fill),
       label: 'Progress',
     ),
   ];
